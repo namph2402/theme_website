159b8aebad2f80a395919cae6a95cb86dc5fb6ee
NamPH <spexi.shop.mail@gmail.com>
2024-04-12 14:35:59 +0700

    add code cart

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,17 +8,17 @@ function promotion(countDownDate) {
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
         if (distance < 0) {
             clearInterval(x);
             days = hours = minutes = seconds = 0;
         }
-        
-        document.getElementById('countdown').innerHTML = 
-            `<div class="promotion-item"><span class="d-block promotion-number">`+ days + `</span>Ngày</div>
-             <div class="promotion-item"><span class="d-block promotion-number">`+ hours +`</span>Giờ</div>
-             <div class="promotion-item"><span class="d-block promotion-number">`+ minutes +`</span>Phút</div>
-             <div class="promotion-item"><span class="d-block promotion-number">`+ seconds +`</span>Giây</div>
+
+        document.getElementById('countdown').innerHTML =
+            `<div class="promotion-item"><span class="d-block promotion-number">` + days + `</span>Ngày</div>
+             <div class="promotion-item"><span class="d-block promotion-number">`+ hours + `</span>Giờ</div>
+             <div class="promotion-item"><span class="d-block promotion-number">`+ minutes + `</span>Phút</div>
+             <div class="promotion-item"><span class="d-block promotion-number">`+ seconds + `</span>Giây</div>
             `;
     }, 1000);
 }
@@ -34,7 +34,7 @@ $(window).scroll(function () {
 
 document.getElementById('scroll').addEventListener('click', () => {
     window.scrollTo({
-      top: 0,
-      behavior: `smooth`
+        top: 0,
+        behavior: `smooth`
     })
-  })
+})
